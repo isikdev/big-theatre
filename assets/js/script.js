022baec514efa20f665c2a101beb989ef992e676
Iskandar <isikjandev@gmail.com>
2024-07-10 16:36:03 +0500

    101%

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.question__accordeon-head').on('click', function () {
+        var $this = $(this);
+        var body = $this.closest('.question__accordeon-item').find('.question__accordeon-body');
+        var img = $this.find('button img');
+
+        $('.question__accordeon-body').not(body).slideUp();
+        $('.question__accordeon-head button img').not(img).removeClass('rotated');
+
+        body.slideToggle();
+        img.toggleClass('rotated');
+    });
+});
+$('.popular__slider-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    dots: true,
+    prevArrow: $('.slick-left-prev'),
+    nextArrow: $('.slick-right-next'),
+});
