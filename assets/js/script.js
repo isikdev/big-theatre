cc0d7ccb252d0e396a0dc8b50ee35a4302d3ea0a
Iskandar <isikjandev@gmail.com>
2024-07-11 19:21:25 +0500

    101%

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,16 @@
-$(document).ready(function () {
-    $('.question__accordeon-head').on('click', function () {
+$(document).ready(function() {
+    var initialAccordion = $('.question__accordeon-item').first();
+    initialAccordion.find('.question__accordeon-body').show();
+    initialAccordion.find('button img').addClass('rotated');
+
+    $('.question__accordeon-head').on('click', function() {
         var $this = $(this);
         var body = $this.closest('.question__accordeon-item').find('.question__accordeon-body');
         var img = $this.find('button img');
-
+        
         $('.question__accordeon-body').not(body).slideUp();
         $('.question__accordeon-head button img').not(img).removeClass('rotated');
-
+        
         body.slideToggle();
         img.toggleClass('rotated');
     });
